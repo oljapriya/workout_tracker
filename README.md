d115bcb58e863134bfe2abd55580fb9706406d66
oljapriya <poljako@gmail.com>
2021-08-09 19:30:55 -0400

    fixing bug

--- README.md
+++ README.md
@@ -24,6 +24,8 @@ The user should be able to:
 
   * View the total duration of each workout from the past seven workouts on the `stats` page.
 
+https://workouttracker09.herokuapp.com/
+
 > **Important:** Look into using a MongoDB aggregate function to dynamically add up and return the total duration for each workout. Check out the [MongoDB documentation on the $addFields](https://docs.mongodb.com/manual/reference/operator/aggregation/addFields/), the [MongoDB documentation on the $sum operator](https://docs.mongodb.com/manual/reference/operator/aggregation/sum/), and the [Mongoose documentation on aggregate functions](https://mongoosejs.com/docs/api.html#aggregate_Aggregate) to learn how it can be accomplished.
 
 To deploy an application with a MongoDB database to Heroku, you'll need to set up a MongoDB Atlas account and connect a database from there to your application. Be sure to use the following guides for support:
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,20 @@
-const router = require ("express").Router();
-const db = require ("../models");
+const router = require("express").Router();
+const db = require("../models");
 
 //Gets workout information
 router.get("/api/workouts", (req, res) => {
   db.Workout.find({})
-   then(dbWorkout => {
-   res.json(dbWorkout);
-  })
-  .catch(err => {
-  res.json(err);
-  })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    })
 });
 
 //Gets workout information for the stats page
 router.get("/api/workouts/range", (req, res) => {
-  db.Workout.find({}).sort({"day": -1}).limit(7)
+  db.Workout.find({}).sort({ "day": -1 }).limit(7)
     .then(dbWorkout => {
       console.log(dbWorkout)
       res.json(dbWorkout);
@@ -25,36 +25,36 @@ router.get("/api/workouts/range", (req, res) => {
 });
 
 //post new workout
-router.post("/api/workouts", ({ body}, res) => {
+router.post("/api/workouts", ({ body }, res) => {
   db.Workout.create(body)
-  .then(dbWorkout => {
-    console.log(dbWorkout)
-    res.json(dbWorkout);
-  })
-  .catch(err => {
-    res.json(err);
-  });
+    .then(dbWorkout => {
+      console.log(dbWorkout)
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 //updates existing workout
 router.put("/api/workouts/:id", (req, res) => {
-  db.Workout.findByIdAndUpdate(req.params.id, {$push: {exercise: req.body}}, {new: true, runValidators: true})
-  .then(dbWorkout => {
-    console.log(dbWorkout)
-    res.json(dbWorkout);
-  });
+  db.Workout.findByIdAndUpdate(req.params.id, { $push: { exercise: req.body } }, { new: true, runValidators: true })
+    .then(dbWorkout => {
+      console.log(dbWorkout)
+      res.json(dbWorkout);
+    });
 });
 
 // Delete a workout
 router.delete("/api/workouts", (req, res) => {
- db.Workout.findByIdDelete(req.body.id)
- .then(() => {
-   res.json(true)
- })
- .catch (err => {
-   res.json(err);
- });
+  db.Workout.findByIdDelete(req.body.id)
+    .then(() => {
+      res.json(true)
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
-module.exports = router; 
+module.exports = router;
 
